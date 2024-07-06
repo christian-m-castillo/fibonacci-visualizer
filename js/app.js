58048c1577a0a4e74094c5e59d446564709698d3
christian-m-castillo <christian.castillo.email@gmail.com>
2024-07-06 16:21:46 -0500

    v1.0

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 function generateFibonacci() {
   const inputNumber = document.getElementById('inputNumber').value;
   const fibonacciSequence = document.getElementById('fibonacciSequence');
-  fibonacciSequence.innerHTMl = '';
+  fibonacciSequence.innerHTML = '';
 
   let fibNumbers = [0, 1];
-  for (let i = 2; i < inputNumbers; i++) {
+  for (let i = 2; i < inputNumber; i++) {
     fibNumbers.push(fibNumbers[i - 1] + fibNumbers[i - 2]);
   }
+
+  fibNumbers.slice(0, inputNumber).forEach(num => {
+    const fibBox = document.createElement('div');
+    fibBox.className = 'fib-box';
+    fibBox.textContent = num;
+    fibonacciSequence.appendChild(fibBox);
+  });
 }
